448f110c0ca710843970e1432242c6e3693ee176
daniel <lesamessipa@gmail.com>
2022-09-15 10:43:20 +0200

    temps d'arret

---
+++ fibo.py
@@ -0,0 +1,34 @@
+# suite de fibonacci
+def fibo(num):
+  nombre_1 = 0
+  nombre_2 = 1
+  total = nombre_1 + nombre_2
+  for i in range(num+1):
+    if i == 0 or i == 1:
+      print("inside")
+      nombre_1 = 0
+      nombre_2 = 1
+    else:
+      print(f" debut : {nombre_1}, {nombre_2}, {total} \n")
+      nombre_1 = nombre_2
+      nombre_2  = total
+      total = nombre_1 + nombre_2
+      print(f" fin: {nombre_1}, {nombre_2}, {total} \n")
+      
+      
+  print(total)
+       
+fibo(5)
+
+def factorielle(n):
+  last_i = 1
+  val = 1
+  response = ""
+  for i in range(1, n+1):
+    # response = f"{last_i} x {i}"
+    print(response)
+    val *=  i
+    last_i = i
+  print(val)
+
+factorielle(4)
--- loop.py
+++ loop.py
@@ -30,12 +30,117 @@ print(f"la somme des element du tableau est :{somme}------")
 
 #le plus grand nombre du tableau
 tableau = [ -1,1, 30, 2, 3, 4, 9, 10]
-maximum = max(tableau)
+maximum = int(max(tableau))
 print(maximum)
 
-# for i in tableau:
-#   if i >= max:
-#     max = i
+for i in tableau:
+  if i >= maximum:
+    maximum = i
 
-# print(f"le plus grand nombre est : {max}")
+print(f"le plus grand nombre est : {max}")
+
+# 2.7 Ecrire un algorithme permettant d’effectuer la multiplication 
+# de deux vecteurs de tailleNconnue `a l’avance et d’afficher le résultat.
+def calcul_vecteur(tab1, tab2):
+  new_list = []
+  for i in range(len(tab1)):
+    val = tab1[i] * tab2[i]
+    new_list.append(val)
+  print(new_list)
+
+tableau = [1, 2, 3]
+tableau2 = [4, 5, 3]
+calcul_vecteur(tableau, tableau2)
+
+#tri Ecrire un algorithme permettant de trier un tableau de taille n
+tableau3 = [4, 5, 3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def tri(tab, mod):
+  isIt = []
+  imp = []
+  for i in range(len(tableau3)):
+    if tableau3[i] % mod :
+      imp.append(tableau3[i])
+    else:
+      isIt.append(tableau3[i])
+  print(f"pair: {isIt}, impair: {imp}")
+
+tri(tableau3, 2)
+
+#multiplication matricielle
+
+a = [3, 4]
+b = [2, 6]
+c = []
+
+for i in range(len(a)):
+  produit = a[i] * b[i]
+  c.append(produit)
+
+print(c)
+#########################
+
+
+# #####################################
+tableau = [1, 5, 20, 30] 
+def calcul_moyenne(tableau):
+  somme = 0
+  index = 0
+  while index < len(tableau) :
+    somme += tableau[index]
+    index += 1
+  
+  moyenne = somme / len(tableau)
+  return moyenne
+  
+la_moyenne = calcul_moyenne(tableau)
+older_moyenne = []
+
+for element in tableau:
+  if (element > la_moyenne):
+    older_moyenne.append(element)
+
+print(older_moyenne, la_moyenne)
+
+def tab_max(arr) -> int:
+  maximum = 0
+  for i in arr:
+    if i > maximum:
+      maximum = i
+  return maximum
+
+tab1 = [1, 3, 5, 10]  
+tab2 = [4, 20, 9, 5]
+
+max_tab_1 = tab_max(tab1)  
+max_tab_2 = tab_max(tab2)
+
+total = max_tab_1 + max_tab_2
+
+print(total)
+
+############ 
+tab1 = [-1, -3, -5, -10]  
+tab2 = [-4, -20, -9, -5]
+
+for i in range(0, len(tab1)):
+  maximum = tab1[0]
+  if maximum <  tab1[i]:
+    maximum = tab1[i]
+  else:
+    maximum = tab1[i]
+
+print(maximum)
+
+#le temps d'arret
+
+number = int(input("entrez une valeur: "))
+somme = 0
+all_values = []
+while number > somme:
+  somme += 1/i
+  all_values.append(round(somme, 2))
+  i+= 1
+
+print(all_values)
+    
 
